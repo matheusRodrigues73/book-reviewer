37c18d1548a1615d9e760e8b9853e1e6ab4d7ebb
Matheus Rodrigues <matheusrodriguesm2104@gmail.com>
2024-12-26 17:39:26 -0300

    refactor: change database communication from `Api` module to respective endpoint

--- app/api/v1/migrations/route.js
+++ app/api/v1/migrations/route.js
@@ -1,27 +1,51 @@
-import Api from "modules/Api";
+import migrationRunner from "node-pg-migrate";
 import { NextResponse } from "next/server";
+import database from "infra/database";
+import { resolve } from "node:path";
 
 // eslint-disable-next-line no-unused-vars
 export async function GET(request) {
+  let client;
   try {
-    const pendentsMigrations = await Api.migrations();
-    return NextResponse.json(pendentsMigrations, { status: 200 });
+    client = await database.clientConnection();
+    const pendingMigrations = await migrationRunner({
+      dbClient: client,
+      dryRun: true,
+      dir: resolve("infra", "migrations"),
+      verbose: true,
+      direction: "up",
+      migrationsTable: "pgmigrations",
+    });
+    console.log("test");
+    return NextResponse.json(pendingMigrations, { status: 200 });
   } catch (error) {
     console.error(error);
     return NextResponse.error(error);
+  } finally {
+    await client.end();
   }
 }
 
 // eslint-disable-next-line no-unused-vars
 export async function POST(request) {
+  let client;
   try {
-    const migratedMigrations = await Api.migrations("post");
-    if (migratedMigrations.length === 0) {
-      return NextResponse.json(migratedMigrations, { status: 200 });
+    client = await database.clientConnection();
+    const migratedMigrations = await migrationRunner({
+      dbClient: client,
+      dir: resolve("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    });
+    if (migratedMigrations.length > 0) {
+      return NextResponse.json(migratedMigrations, { status: 201 });
     }
-    return NextResponse.json(migratedMigrations, { status: 201 });
+    return NextResponse.json(migratedMigrations, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.error(error);
+  } finally {
+    client.end();
   }
 }
--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,11 +1,20 @@
 import { NextResponse } from "next/server";
-import Api from "modules/Api.js";
+import database from "infra/database";
 
 // eslint-disable-next-line no-unused-vars
 export async function GET(request) {
   try {
-    const { updatedAt, version, maxConnections, openedConnections } =
-      await Api.status();
+    const updatedAt = new Date().toISOString();
+    const versionResponse = await database.query("SHOW server_version;");
+    const maxConnectionsResponse = await database.query(
+      "SHOW max_connections;",
+    );
+    const openedConnectionsResponse = await database.query(
+      "SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db';",
+    );
+    const version = versionResponse.rows[0].server_version;
+    const maxConnections = maxConnectionsResponse.rows[0].max_connections;
+    const openedConnections = openedConnectionsResponse.rows[0].count;
     return NextResponse.json(
       {
         updated_at: updatedAt,
