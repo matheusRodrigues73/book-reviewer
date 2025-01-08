a4f3600aa2929442e19e604fcb59155416a94c0e
Matheus Rodrigues <matheusrodriguesm2104@gmail.com>
2025-01-08 18:48:41 -0300

    feat: make custom errors in `errors.js`

--- infra/errors.js
+++ infra/errors.js
@@ -12,7 +12,47 @@ export class InternalServerError extends Error {
       name: this.name,
       message: this.message,
       action: this.action,
-      statusCode: this.statusCode,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class InvalidMethodError extends Error {
+  constructor({ method, validMethods }) {
+    super(`method ${method} is not allowed`, { method, validMethods });
+    this.name = "InvalidMethodError";
+    this.action = `Only this methods are allowed: ${validMethods}`;
+    this.statusCode = 405;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class InvalidParamsError extends Error {
+  constructor({ nullParams, invalidFormat }) {
+    super(
+      `${nullParams ? `${nullParams} cannot be Null,` : ""}${invalidFormat ? `${invalidFormat} are missed formatted` : ""} could not submit datas`,
+    );
+    this.name = "InvalidParamsError";
+    this.action = `${nullParams ? `Fill params: ${nullParams};` : ""}${invalidFormat ? `Format params: ${invalidFormat}` : ""}`;
+    this.statusCode = 400;
+    this.nullParams = nullParams;
+    this.invalidFormat = invalidFormat;
+  }
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      nullParams: this.nullParams,
+      invalidFormat: this.invalidFormat,
+      status_code: this.statusCode,
     };
   }
 }
